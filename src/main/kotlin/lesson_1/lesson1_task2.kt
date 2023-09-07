668dad4558c79500510522315fda220efbafc9a4
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-07 17:03:13 +0600

    KS-1-2 Added new variable with employee numbers and printed it out

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+    var volumeOfOrders: Int = 75
+    val greetCustomer: String = "Thank you for order. Good Luck!!!"
+
+    println(volumeOfOrders)
+    println(greetCustomer)
+
+
+    var storeEmployeesNumber : Int = 2000
+
+    //println(storeEmployeesNumber)
+
+    storeEmployeesNumber--
+
+    println(storeEmployeesNumber)
+}
