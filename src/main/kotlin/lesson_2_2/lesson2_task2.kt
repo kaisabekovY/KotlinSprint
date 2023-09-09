aa322dbb3310c15e898cfe8cf50fcb46ebce1003
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-09 15:03:42 +0600

    KS-2-2 Проставил пробелы в нужных местах

--- src/main/kotlin/lesson_2_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2_2/lesson2_task2.kt
@@ -1,21 +1,21 @@
 package lesson_2_2
 
 fun main() {
-    var employeesNumber: Int = 50
+    var employeesNumber : Int = 50
     var employeeSalary : Int = 30000
-    var internsNumber: Int = 20
-    var internsSalary: Int = 20000
+    var internsNumber : Int = 20
+    var internsSalary : Int = 20000
 
-    val salaryTotalOfEmployees: Int = employeeSalary*employeesNumber
-    val salaryTotalOfInterns: Int = internsSalary*internsNumber
-    val totalExpenseOfCompany: Int = salaryTotalOfEmployees + salaryTotalOfInterns
+    val salaryTotalOfEmployees : Int = employeeSalary * employeesNumber
+    val salaryTotalOfInterns : Int = internsSalary * internsNumber
+    val totalExpenseOfCompany : Int = salaryTotalOfEmployees + salaryTotalOfInterns
 
     //Sum new employees
-    employeesNumber+= internsNumber
+    employeesNumber += internsNumber
 
-    val averageSalaryPerEmployee: Int = totalExpenseOfCompany/employeesNumber
+    val averageSalaryPerEmployee: Int = totalExpenseOfCompany / employeesNumber
 
     println("Overall expenses before new employees: $salaryTotalOfEmployees")
-    println("Overall expenses after new employees: $totalExpenseOfCompany")
+    println("Overall expenses after new employees : $totalExpenseOfCompany")
     println("Average salary per employee: $averageSalaryPerEmployee")
 }
