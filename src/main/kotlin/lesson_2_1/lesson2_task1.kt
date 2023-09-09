d2746fde0cebd5a274a02b3e052567fe70caafd4
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-09 14:57:23 +0600

    KS-2-1 Проставил пробелы и использовал функцию average()

--- src/main/kotlin/lesson_2_1/lesson2_task1.kt
+++ src/main/kotlin/lesson_2_1/lesson2_task1.kt
@@ -1,12 +1,12 @@
 package lesson_2_1
 
 fun main() {
-    val firstStudentMark: Int = 3
-    val secondStudentMark: Int = 4
-    val thirdStudentMark: Int = 3
-    val fourthStudentMark: Int = 5
+    val firstStudentMark : Int = 3
+    val secondStudentMark : Int = 4
+    val thirdStudentMark : Int = 3
+    val fourthStudentMark : Int = 5
 
-    val averageMarkOfStudents: Double = (firstStudentMark+secondStudentMark+thirdStudentMark+fourthStudentMark).toDouble()/4
+    val marksList : List<Int> = listOf(firstStudentMark,secondStudentMark,thirdStudentMark,fourthStudentMark)
 
-    println(String.format("%.2f",averageMarkOfStudents))
+    println(String.format("%.2f",marksList.average()))
 }
