0556544a05436d0d449b6fbdf60a3b43e768603a
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-09 15:17:09 +0600

    KS-2-3 Проинициализировал время в пути по условию задачи

--- src/main/kotlin/lesson_2_3/lesson2_task3.kt
+++ src/main/kotlin/lesson_2_3/lesson2_task3.kt
@@ -1,15 +1,15 @@
 package lesson_2_3
 
 fun main() {
-    val trainDepartureHour: Int = 9
-    val trainDepartureMinute: Int = 39
-    val travelTime: Int = 1000
+    val trainDepartureHour : Int = 9
+    val trainDepartureMinute : Int = 39
+    val travelTime : Int = 457
 
-    val departureTimeInMinutes: Int = (trainDepartureHour * 60) + trainDepartureMinute
-    val arrivalTimeInMinutes: Int = departureTimeInMinutes + travelTime
+    val departureTimeInMinutes : Int = (trainDepartureHour * 60) + trainDepartureMinute
+    val arrivalTimeInMinutes : Int = departureTimeInMinutes + travelTime
 
-    val trainArrivalHour: Int = if(arrivalTimeInMinutes / 60 > 24) (arrivalTimeInMinutes / 60) % 24 else arrivalTimeInMinutes / 60
-    val trainArrivalMinute: Int = arrivalTimeInMinutes % 60
+    val trainArrivalHour : Int = if(arrivalTimeInMinutes / 60 > 24) (arrivalTimeInMinutes / 60) % 24 else arrivalTimeInMinutes / 60
+    val trainArrivalMinute : Int = arrivalTimeInMinutes % 60
 
     val arrivalTime = String.format("%02d:%02d",trainArrivalHour,trainArrivalMinute)
 
