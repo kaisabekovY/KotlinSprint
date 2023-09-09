2cbe2e5206ce469d048b24295f4dff05fb58eff8
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-09 20:25:38 +0600

    KS-3-1 added greeting depending on hour using LocalDateTime

---
+++ src/main/kotlin/lesson_3_1/lesson3_task1.kt
@@ -0,0 +1,20 @@
+package lesson_3_1
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    val formatter : DateTimeFormatter = DateTimeFormatter.ofPattern("H")
+    val hoursNow: Int = LocalDateTime.now().format(formatter).toInt()
+
+    val name: String = "Eldar"
+    val space: Char = ' '
+
+    val greeting: String = when {
+        hoursNow in 11..18 -> "Good Afternoon!"
+        hoursNow in 19..24 -> "Good Evening!"
+        else -> "Good Morning"
+    }
+
+    println("$greeting$space$name!")
+}
