bbb50330912bd7751410f662e8644c251aa322a1
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-10 19:11:22 +0600

    KS-3-4 deleted unnecessary brackets

--- src/main/kotlin/lesson_3_4/lesson3_task4.kt
+++ src/main/kotlin/lesson_3_4/lesson3_task4.kt
@@ -5,11 +5,11 @@ fun main() {
     var from: String = "E2"
     var where: String = "E4"
 
-    println("[$from-$where;$moveNumber]")
+    println("$from-$where;$moveNumber")
 
     moveNumber = 2
     from = "D2"
     where = "D3"
 
-    println("[$from-$where;$moveNumber]")
+    println("$from-$where;$moveNumber")
 }
