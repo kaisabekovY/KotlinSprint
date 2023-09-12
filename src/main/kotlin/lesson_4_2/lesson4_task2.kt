abc7a7a030320451715f48596bd5b54afa8409cc
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-12 09:43:55 +0600

    KS-4-2 Вынес в константу 3 переменные maxWeight, minWeight и maxVolume

--- src/main/kotlin/lesson_4_2/lesson4_task2.kt
+++ src/main/kotlin/lesson_4_2/lesson4_task2.kt
@@ -1,16 +1,17 @@
 package lesson_4_2
 
-fun main() {
-    val maxWeight: Int = 100
-    val minWeight: Int = 35
+const val maxWeight: Int = 100
+const val minWeight: Int = 35
+const val maxVolume: Int = 100
 
+fun main() {
     var weight: Int = 20
     var volume: Int = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < maxVolume)}")
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < maxVolume)}")
 }
