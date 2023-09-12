9eeb36ed891f32595cd0680a11099c657c4d0b12
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-12 22:56:01 +0600

    KS-4-2 Заглавные буквы в константах

--- src/main/kotlin/lesson_4_2/lesson4_task2.kt
+++ src/main/kotlin/lesson_4_2/lesson4_task2.kt
@@ -1,17 +1,17 @@
 package lesson_4_2
 
-const val maxWeight: Int = 100
-const val minWeight: Int = 35
-const val maxVolume: Int = 100
+const val MAX_WEIGHT: Int = 100
+const val MIN_WEIGHT: Int = 35
+const val MAX_VOLUME: Int = 100
 
 fun main() {
     var weight: Int = 20
     var volume: Int = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < maxVolume)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= MIN_WEIGHT && weight <= MAX_WEIGHT) and (volume < MAX_VOLUME)}")
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < maxVolume)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= MIN_WEIGHT && weight <= MAX_WEIGHT) and (volume < MAX_VOLUME)}")
 }
