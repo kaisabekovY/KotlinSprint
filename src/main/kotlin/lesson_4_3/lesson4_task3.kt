a25344ccd5cdd0bbe97e7e7a54cb89d7641afc6f
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-12 23:04:26 +0600

    KS-4-3 Сделал заглавные буквы в константах и укоротил строку

--- src/main/kotlin/lesson_4_3/lesson4_task3.kt
+++ src/main/kotlin/lesson_4_3/lesson4_task3.kt
@@ -1,15 +1,18 @@
 package lesson_4_3
 
-const val season: String = "Winter"
-const val airHumidity: Int = 20
-const val isAwningOpen: Boolean = true
-const val isSunny: Boolean = true
+const val SEASON: String = "Winter"
+const val AIR_HUMIDITY: Int = 20
+const val IS_AWNING_OPEN: Boolean = true
+const val IS_SUNNY: Boolean = true
 
 fun main() {
     val seasonNow: String = "Winter"
     val airHumidityNow: Int = 20
     val isAwningOpenNow: Boolean = true
     val isSunnyNow: Boolean = true
-    println("Благоприятные ли сейчас условия для роста бобовых? ${season != seasonNow && airHumidity == airHumidityNow && isAwningOpen == isAwningOpenNow && isSunny == isSunnyNow}")
+
+    val areConditionsSatisfies: Boolean = SEASON != seasonNow && AIR_HUMIDITY == airHumidityNow && IS_AWNING_OPEN == isAwningOpenNow && IS_SUNNY == isSunnyNow
+
+    println("Благоприятные ли сейчас условия для роста бобовых? $areConditionsSatisfies")
 }
 
