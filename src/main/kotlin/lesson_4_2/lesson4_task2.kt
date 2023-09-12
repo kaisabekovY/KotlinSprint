3f410ce88501a73987abb774aa3196d9f1d778e8
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-12 09:36:55 +0600

    KS-4-2 Вынес максимальный и минимальный вес в отдельную переменную + сделал объем не включительно 100

--- src/main/kotlin/lesson_4_2/lesson4_task2.kt
+++ src/main/kotlin/lesson_4_2/lesson4_task2.kt
@@ -1,13 +1,16 @@
 package lesson_4_2
 
 fun main() {
+    val maxWeight: Int = 100
+    val minWeight: Int = 35
+
     var weight: Int = 20
     var volume: Int = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= 35 && weight <= 100) and (volume <= 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < 100)}")
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= 35 && weight <= 100) and (volume <= 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight >= minWeight && weight <= maxWeight) and (volume < 100)}")
 }
