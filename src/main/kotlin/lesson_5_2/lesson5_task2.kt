d77652fda2684ffd813c8bebe8d6a37f68937da2
KaisabekovY <kaisabekov.yeldar@outlook.com>
2023-09-12 14:17:27 +0600

    KS-5-2 Добавил проверку на возраст

---
+++ src/main/kotlin/lesson_5_2/lesson5_task2.kt
@@ -0,0 +1,23 @@
+package lesson_5_2
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+const val minAge = 18
+
+fun main() {
+    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("Y")
+    val currentYear: Int = LocalDateTime.now().format(formatter).toInt()
+
+    println("Enter your year of birth")
+    var yearOfBirth: Int? = readlnOrNull()?.toInt()
+    if(yearOfBirth == null) {
+        yearOfBirth = 0
+    }
+
+    if(currentYear - yearOfBirth >= minAge) {
+        println("Показать экран со скрытым контентом")
+    } else {
+        println("Вам не исполнилось 18")
+    }
+}
